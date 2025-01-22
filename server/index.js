ba63670f1c816386fc8197d310521659eaf97726
bpsingh0802 <80077777+bpsingh0802@users.noreply.github.com>
2025-01-22 10:45:24 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -70,21 +70,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 
 
-// app.post("/signup", async (req, res) => {
-//     try {
-//         const { name, email, password } = req.body;
-//         const existingUser = await UserModel.findOne({ email });
-//         if (existingUser) {
-//             return res.status(400).json({ error: "Email already exists" });
-//         }
-//         const hashedPassword = await bcrypt.hash(password, 10);
-//         const newUser = new UserModel({ name, email, password: hashedPassword });
-//         const savedUser = await newUser.save();
-//         res.status(201).json(savedUser);
-//     } catch (error) {
-//         res.status(500).json({ error: error.message });
-//     }
-// });
+
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, "uploads/"); // Directory to save uploaded images
@@ -144,27 +130,6 @@ app.post("/signup", upload.array("images", 5), async (req, res) => {
 });
 
 
-// app.post("/login", async (req, res) => {
-//     try {
-//         const { email, password } = req.body;
-//         const user = await UserModel.findOne({ email });
-//         if (user) {
-//             const passwordMatch = await bcrypt.compare(password, user.password);
-//             if (passwordMatch) {
-//                 req.session.user = { id: user._id, name: user.name, email: user.email };
-//                 // console.log(email);
-//                 console.log(user.name);
-//                 res.json("Success");
-//             } else {
-//                 res.status(401).json("Password doesn't match");
-//             }
-//         } else {
-//             res.status(404).json("No Records found");
-//         }
-//     } catch (error) {
-//         res.status(500).json({ error: error.message });
-//     }
-// });
 
 app.post("/login", async (req, res) => {
     try {
